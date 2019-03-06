4b7a5e963f63d5f43eae1577421b68d5f07a1a05
YFPW <43461143+YFPW@users.noreply.github.com>
2019-03-07 10:49:29 +1100

    Delete IRemoteClient.java

--- src/remo/IRemoteClient.java
+++
@@ -1,8 +0,0 @@
-package remo;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IRemoteClient extends Remote{
-	public void showName(String userName) throws RemoteException;
-}
